255fb7376fdb8a0a3d29246d947fd29dea331fcd
Otávio Schwanck dos Santos <otavioschwanck@gmail.com>
2022-03-23 13:00:15 -0300

    fix gitignore

--- lua/user-plugins.lua
+++
@@ -1,8 +0,0 @@
--- Add your plugins here
-
-local user_plugins = {
-  -- 'kat0h/nyancat.vim',
-  { 'glacambre/firenvim', run = function() vim.fn['firenvim#install'](0) end },
-}
-
-return user_plugins
