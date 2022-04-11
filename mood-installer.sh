6024750072f6dcc842a8460d3575825b194bba6e
Túlio Magnus <tuliokpmagnus@gmail.com>
2022-04-11 18:32:12 -0300

    almost there

--- mood-installer.sh
+++ mood-installer.sh
@@ -1,17 +1,21 @@
-#!/bin/bash
+ #!/bin/bash
 APT_PACKAGES=(sqlite3 libsqlite3-dev neovim xclip python3-pip)
 NPM_PACKAGES=(neovim diagnostic-languageserver)
 RUBY_VERSIONS=(2.7.1 2.7.3 3.1.1 3.0.3)
 GEMS=(solargraph neovim bundler)
 export LAZY_VER="0.31.4" # LAZYGIT VERSION
 
-# SCRIPT QUESTIONAIRE
-ask_question "base packages for neovim" install_packages
-ask_question "Ruby on Rails with Rbenv" install_ruby
-ask_question "LazyGit" install_lazygit
-install_fonts
-install_gems
-install_nvim
+get_machine_type () {
+  unameOut="$(uname -s)"
+  case "${unameOut}" in
+    Linux*)     machine=Linux;;
+    Darwin*)    machine=Mac;;
+    CYGWIN*)    machine=Cygwin;;
+    MINGW*)     machine=MinGw;;
+    *)          machine="UNKNOWN:${unameOut}"
+  esac
+  echo "Proceeding instalation for OS ${machine}"
+}
 
 ask_question () {
   echo "Do you wish to install $1?"
@@ -22,15 +26,21 @@ ask_question () {
     esac
   done
 }
+
 # Install base packages
-install_packages () {
+install_packages_linux () {
   echo "================= INSTALLING PACKAGES ================="
   sudo apt-get -qq update
   sudo apt-get -qq install ${APT_PACKAGES[*]} -y
   sudo npm install -s -g ${NPM_PACKAGES[*]} -y
 }
 
-install_ruby () {
+install_packages_mac () {
+  echo "================= INSTALLING PACKAGES ================="
+  brew install sqlite
+}
+
+install_ruby_linux () {
   echo "================= INSTALLING RUBY ================="
   curl -fsSL https://github.com/rbenv/rbenv-installer/raw/HEAD/bin/rbenv-installer | bash
   git clone https://github.com/rbenv/ruby-build.git
@@ -38,6 +48,10 @@ install_ruby () {
   PREFIX=/usr/local sudo ./ruby-build/install.sh
   echo "gem: --no-document" > ~/.gemrc
   for i in $RUBY_VERSIONS; do rbenv install $i -s; echo "Installed ruby version $i"; done
+}
+
+install_ruby_mac () {
+  echo "================= TODO: INSTALLING RUBY ON MAC ================="
 }
 
 install_fonts () {
@@ -74,11 +88,41 @@ install_gems () {
   for i in $GEMS; do gem install i --silent; done
 }
 
-install_lazygit () {
+install_lazygit_linux () {
   echo "================= INSTALLING LAZY GIT ================="
   wget -q -O lazygit.tgz https://github.com/jesseduffield/lazygit/releases/download/v${LAZY_VER}/lazygit_${LAZY_VER}_Linux_x86_64.tar.gz
   tar xf lazygit.tgz
   sudo mv lazygit /usr/local/bin/
 }
+
+install_lazygit_mac () {
+  echo "================= INSTALLING LAZY GIT ================="
+  brew install jesseduffield/lazygit/lazygit
+  brew install lazygit
+}
+
+linux_workflow () {
+  ask_question "base packages for neovim" install_packages_linux
+  ask_question "Ruby on Rails with Rbenv" install_ruby_linux
+  ask_question "LazyGit" install_lazygit_linux
+}
+
+mac_workflow () {
+  ask_question "base packages for neovim" install_packages_mac
+  ask_question "Ruby on Rails with Rbenv" install_ruby_mac
+  ask_question "LazyGit" install_lazygit_mac
+}
+
+# SCRIPT QUESTIONAIRE
+get_machine_type
+case "${machine}" in
+  Linux)     linux_workflow;;
+  Darwin)    mac_workflow;;
+  *)         echo "OS not recognized"
+esac
+
+install_fonts
+install_gems
+install_nvim
 
 echo "Script finished!"
