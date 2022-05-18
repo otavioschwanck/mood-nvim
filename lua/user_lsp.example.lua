789eb622fe2d358c7b931879038a74805cf1deb8
Otávio Schwanck dos Santos <otavioschwanck@gmail.com>
2022-05-18 01:32:14 +0100

    fix scroll

--- lua/user_lsp.example.lua
+++ lua/user_lsp.example.lua
@@ -64,8 +64,8 @@ cmp.setup({
       end,
       { "i", "s", --[[ "c" (to enable the mapping in command mode) ]] }
     ),
-    ['<C-d>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
-    ['<C-u>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
+    ['<C-u>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
+    ['<C-d>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
     ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
     ['<C-g>'] = cmp.mapping({ i = cmp.mapping.abort(), c = cmp.mapping.close(), }),
     ['<CR>'] = cmp.mapping.confirm({ select = false }),
