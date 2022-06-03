c259d02e766054446f7639b68eb860e96717f380
Otávio Schwanck dos Santos <otavioschwanck@gmail.com>
2022-06-03 14:16:23 +0100

    improve harpoon (a lot)

--- lua/harpoon-menu.lua
+++ lua/harpoon-menu.lua
@@ -1,5 +1,26 @@
 local wk = require("which-key")
 
+local function Split(s, delimiter)
+  result = {};
+
+  for match in (s..delimiter):gmatch("(.-)"..delimiter) do
+    table.insert(result, match);
+  end
+  return result;
+end
+
+local function count_in_list(list, element)
+  local count = 0
+
+  for i=1,#list,1 do
+    if list[i] == element then
+      count = count + 1
+    end
+  end
+
+  return count
+end
+
 local function toggle_harpoon()
   local harpoon = require("harpoon")
   local index = 1
@@ -9,12 +30,42 @@ local function toggle_harpoon()
     f = { ':lua require("harpoon.ui").toggle_quick_menu()<CR>', "Open Quick Menu" },
     c = { ':lua require("harpoon.mark").clear_all()<CR>', "Clear Harpoon" }
   }
-  local config = harpoon.get_mark_config().marks[index]
+
+  local marks = harpoon.get_mark_config().marks
+  local config = marks[index]
+
+  local filenames = {}
+
+  for i=1,#marks,1 do
+    local file_splitted = Split(marks[i].filename, "/")
+
+    filenames[i] = file_splitted[#file_splitted]
+  end
+
+  print(vim.inspect(filenames))
 
   while config do
-    harpoons[tostring(index)] = { ":lua require'harpoon.ui'.nav_file(" .. index .. ")<CR>", config.filename }
+    local file_to_show
+
+    if count_in_list(filenames, filenames[index]) > 1 then
+      local filename_full = Split(config.filename, "/")
+
+      table.remove(filename_full, #filename_full)
+
+      local parsed_filename = ""
+
+      for i=1,#filename_full,1 do
+        parsed_filename = parsed_filename .. "/" .. filename_full[i]
+      end
+
+      file_to_show = filenames[index] .. ' at ' .. parsed_filename
+    else
+      file_to_show = filenames[index]
+    end
+
+    harpoons[tostring(index)] = { ":lua require'harpoon.ui'.nav_file(" .. index .. ")<CR>", file_to_show }
     index = index + 1
-    config = harpoon.get_mark_config().marks[index]
+    config = marks[index]
   end
 
   local ig_index = 1
@@ -24,8 +75,6 @@ local function toggle_harpoon()
     ignored[tostring(ig_index)] = "which_key_ignore"
     ig_index = ig_index + 1
   end
-
-  print(vim.inspect(ignored))
 
   wk.register({
     y = ignored,
