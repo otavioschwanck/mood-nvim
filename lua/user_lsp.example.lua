f74a540deec6775c24160e0da3e809eb18bda8c4
Otávio Schwanck dos Santos <otavioschwanck@gmail.com>
2022-06-18 15:24:18 +0100

    fix user_Lsp

--- lua/user_lsp.example.lua
+++ lua/user_lsp.example.lua
@@ -91,9 +91,6 @@ cmp.setup({
 local cmp_autopairs = require('nvim-autopairs.completion.cmp')
 cmp.event:on( 'confirm_done', cmp_autopairs.on_confirm_done({  map_char = { tex = '' } }))
 
--- add a lisp filetype (wrap my-function), FYI: Hardcoded = { "clojure", "clojurescript", "fennel", "janet" }
-cmp_autopairs.lisp[#cmp_autopairs.lisp+1] = "racket"
-
 local servers = { 'pyright', 'tsserver', 'jsonls', 'html', 'yamlls', 'cssls', 'solidity_ls', 'sumneko_lua' }
 
 require "lsp_signature".setup()
