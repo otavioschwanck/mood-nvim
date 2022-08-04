c342964253ea3a944fa65bc77c2d795f17cfccd1
Otávio Schwanck dos Santos <otavioschwanck@gmail.com>
2022-08-04 11:44:22 +0100

    remove user plugins example

--- lua/user-plugins.example.lua
+++
@@ -1,9 +0,0 @@
--- Add your plugins here
-
-local user_plugins = {
-  -- 'kat0h/nyancat.vim',
-  { 'glacambre/firenvim', run = function() vim.fn['firenvim#install'](0) end },
-  -- { 'romgrk/barbar.nvim' } -- Enable tabs?
-}
-
-return user_plugins
