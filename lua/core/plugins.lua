e1b55af7de1a2d962facf1d9b0c78289a1072587
Otávio Schwanck dos Santos <otavioschwanck@gmail.com>
2022-08-08 17:34:35 +0100

    remove virtual text

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -13,7 +13,6 @@ function M.setup()
     -- Debug
     use { 'mfussenegger/nvim-dap' }
     use { 'rcarriga/nvim-dap-ui' }
-    use { 'theHamsta/nvim-dap-virtual-text' }
     use { 'nvim-telescope/telescope-dap.nvim' }
     use { "mxsdev/nvim-dap-vscode-js" }
     use {
