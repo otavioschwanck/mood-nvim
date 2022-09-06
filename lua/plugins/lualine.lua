be088ddac28388f72baaac486682b8f065525a56
Otávio Schwanck dos Santos <otavioschwanck@gmail.com>
2022-09-06 13:28:19 +0100

    diagnostic is coc

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -222,7 +222,7 @@ if windwidth <= 84 or #data > estimated_space_available then
 
   ins_left_both {
     'diagnostics',
-    sources = { 'nvim_diagnostic' },
+    sources = { 'coc' },
     symbols = { error = ' ', warn = ' ', info = ' ' },
     diagnostics_color = {
       color_error = { fg = colors.red },
