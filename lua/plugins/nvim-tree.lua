6d2f64034d503fb523b90608def34dcd0d9970b8
Otávio Schwanck dos Santos <otavioschwanck@gmail.com>
2022-09-08 18:12:47 +0100

    adaptive size

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -4,6 +4,7 @@ function M.setup()
   require("nvim-tree").setup({
     update_cwd = false,
     view = {
+      adaptive_size = true,
       hide_root_folder = true,
       mappings = {
         custom_only = false,
