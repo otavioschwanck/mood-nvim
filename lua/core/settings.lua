c8ee1aea1b5812657fff19fe32b64a99046c1641
Otávio Schwanck dos Santos <otavioschwanck@gmail.com>
2022-09-11 21:42:44 +0100

    improve whells

--- lua/core/settings.lua
+++ lua/core/settings.lua
@@ -1,7 +1,9 @@
 local M = {}
 
-function M.show_message(expected, instead, goToInsert)
-  if goToInsert then
+function M.show_message(expected, instead, goToInsert, hasCtrl)
+  if goToInsert and hasCtrl then
+    require('notify')('Use Ctrl-' .. expected .. ' or go to insert and use ' .. expected .. ' instead of ' .. instead .. '.', 'warn', { title = 'Bycicle without small whells' })
+  elseif goToInsert then
     require('notify')('Go to insert and use ' .. expected .. ' instead of ' .. instead .. '.', 'warn', { title = 'Bycicle without small whells' })
   else
     require('notify')('Use ' .. expected .. ' instead of ' .. instead .. '.', 'warn', { title = 'Bycicle without small whells' })
@@ -11,8 +13,8 @@ end
 function M.remove_bicycle_small_whells(opts)
   local set = vim.keymap.set
 
-  set('i', '<Left>', function() M.show_message("H", "Left Arrow", true) end)
-  set('i', '<Right>', function() M.show_message("L", "Right Arrow", true) end)
+  set('i', '<Left>', function() M.show_message("H", "Left Arrow", true, true) end)
+  set('i', '<Right>', function() M.show_message("L", "Right Arrow", true, true) end)
   set('i', '<Up>', function() M.show_message("K", "Up Arrow", true) end)
   set('i', '<Down>', function() M.show_message("J", "Down Arrow", true) end)
 
