143f7937da860f19482f9902b85833b782340736
Otávio Schwanck dos Santos <otavioschwanck@gmail.com>
2022-11-08 12:17:31 +0000

    remove for default the format

--- extra/examples/config.lua
+++ extra/examples/config.lua
@@ -98,8 +98,8 @@ local four_space_languages = { "solidity" }
 local autocommands = {
   { {"FileType"}, two_space_languages, function() vim.cmd('setlocal shiftwidth=2 tabstop=2') end },
   { {"FileType"}, four_space_languages, function() vim.cmd('setlocal shiftwidth=4 tabstop=4') end },
-  { {'BufWritePre'}, {"*.tsx", "*.ts", "*.jsx", "*.js"}, function() vim.cmd("Prettier") end, },
-  { {'BufWritePre'}, {"*.rb"}, function() vim.lsp.buf.format { async = false, filter = function(client) return client.name == "solargraph" end } end, }
+  -- { {'BufWritePre'}, {"*.tsx", "*.ts", "*.jsx", "*.js"}, function() vim.cmd("Prettier") end, }, -- prettifer on save
+  -- { {'BufWritePre'}, {"*.rb"}, function() vim.lsp.buf.format { async = false, filter = function(client) return client.name == "solargraph" end } end, } -- rubocop on save
 }
 
 for i = 1, #autocommands, 1 do
