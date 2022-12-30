cc6aafd74196a22fdc2f38006989c3ddbff886eb
Otávio Schwanck dos Santos <otavioschwanck@gmail.com>
2022-12-30 13:52:56 +0000

    improve load

--- init.lua
+++ init.lua
@@ -1,37 +1,65 @@
 -- setup user files
-require('mood-scripts.install-config').setup()
+local not_ok = {}
 
-require('impatient')
+local function setup(path)
+  local ok, res = pcall(require(path).setup)
+
+  if not(ok) then
+    table.insert(not_ok, path)
+
+    if os.getenv("DEBUG") == "true" then
+      print("[WARN] [" .. path .. "]: " .. res)
+    end
+  end
+end
+
+setup('mood-scripts.install-config')
+
+pcall(require, 'impatient')
 
 -- vim script functions
-require('helpers.vim-functions').setup()
-require('helpers.term-functions').setup()
+setup('helpers.vim-functions')
+setup('helpers.term-functions')
 
-require('core.plugins').setup()
-require('core.autocmds').setup()
-require('core.set').setup()
-require('core.globals').setup()
-require('core.mappings').setup()
+setup('core.plugins')
+
+if #not_ok == 0 then
+  setup('core.autocmds')
+end
+
+setup('core.set')
+setup('core.globals')
+setup('core.mappings')
 
 -- mood
-require('mood-scripts.quick-consult').setup()
+setup('mood-scripts.quick-consult')
 
 -- plugins
-require('plugins.telescope').setup()
-require('plugins.tree-sitter').setup()
-require('plugins.autopairs').setup()
-require('plugins.yanky').setup()
-require('plugins.dashboard_plugin').setup()
-require('plugins.lualine').setup()
-require('plugins.ctrlsf').setup()
-require('plugins.ror').setup()
+setup('plugins.telescope')
+setup('plugins.tree-sitter')
+setup('plugins.autopairs')
+setup('plugins.yanky')
+setup('plugins.dashboard_plugin')
+setup('plugins.lualine')
+setup('plugins.ctrlsf')
+setup('plugins.ror')
 
 -- user
 pcall(require, 'user.lsp')
 pcall(require, 'user.config')
 pcall(require, 'user.keybindings')
 
-require('plugins.nvim-tree').setup()
+setup('plugins.nvim-tree')
 
 -- startup
-require('core.start').setup()
+setup('core.start')
+
+if #not_ok > 0 then
+  print("Some Error happening when loading neovim: \n")
+  print("Try to restart Neovim.  If the error persist:\n1 - Run :PackerInstall\n2 - Run :UpdateMood\n3 - Try to reinstall neovim.\n4 - Create an issue to help to fix\n5 - run neovim with DEBUG=true nvim")
+  print("Modules with errors: ")
+
+  for i = 1, #not_ok, 1 do
+    print(" - " .. not_ok[i])
+  end
+end
