f7e0690cec7849ae59a7ba0c8eda6d60ff50e061
Otávio Schwanck dos Santos <otavioschwanck@gmail.com>
2023-01-02 02:38:32 +0000

    typescript plugin

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -146,6 +146,7 @@ function M.setup()
     }
 
     use {'nvim-telescope/telescope-ui-select.nvim' }
+    use 'jose-elias-alvarez/typescript.nvim'
 
     use {
       'nvim-telescope/telescope.nvim',
@@ -155,6 +156,25 @@ function M.setup()
 
   vim.cmd("PackerInstall")
   vim.cmd("PackerClean")
+
+  require("typescript").setup({
+    disable_commands = false, -- prevent the plugin from creating Vim commands
+    debug = false, -- enable debug logging for commands
+    go_to_source_definition = { fallback = true },
+    server = {
+      on_attach = function(client, bufnr)
+        vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+        local bufopts = { noremap=true, silent=true, buffer=bufnr }
+        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+        vim.keymap.set('n', 'gd', ':Telescope lsp_definitions<CR>', bufopts)
+        vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+        vim.keymap.set('n', 'gI', vim.lsp.buf.implementation, bufopts)
+        vim.keymap.set('n', 'gt', vim.lsp.buf.type_definition, bufopts)
+        vim.keymap.set('n', 'gr', ':Telescope lsp_references<CR>', bufopts)
+      end
+    }
+  })
 
   require('gitsigns').setup()
 
