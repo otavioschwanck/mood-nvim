f7558c7056d30be3ae451d507c3ce746fd07910b
Otávio Schwanck dos Santos <otavioschwanck@gmail.com>
2023-01-23 02:40:07 +0000

    improve lsp config

--- extra/examples/lsp.lua
+++ extra/examples/lsp.lua
@@ -1,32 +1,47 @@
-local lsp = require('lsp-zero')
+  require("luasnip.loaders.from_vscode").lazy_load()
+  require("luasnip.loaders.from_vscode").lazy_load({ paths = { "./vs-snippets" } })
 
-lsp.preset('recommended')
+  local lsp = require('lsp-zero')
 
-lsp.ensure_installed({
-  'tsserver',
-  'eslint',
-  'sumneko_lua',
-  'solargraph',
-  'jsonls',
-  'solidity'
-})
+  lsp.preset('recommended')
 
-lsp.configure('solidity', {
-  settings = {
-    solidity = { includePath = '',
-      remapping = { ["@OpenZeppelin/"] = 'dependencies/OpenZeppelin/openzeppelin-contracts@4.6.0/' } }
-  }
-})
+  local cmp = require('cmp')
+  local luasnip = require("luasnip")
 
-lsp.nvim_workspace()
+  local cmp_mappings = lsp.defaults.cmp_mappings({
+    ['<C-d>'] = cmp.mapping(function (fallback)
+      if luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      else
+        fallback()
+      end
+    end),
+  })
 
-local sources = require('lsp-zero').defaults.cmp_sources()
+  lsp.ensure_installed({
+    'tsserver',
+    'eslint',
+    'sumneko_lua',
+    'solargraph',
+    'jsonls',
+    'solidity'
+  })
 
-table.insert(sources, { name = "calc" }) -- adding cmp-calc.  Try to type 3 + 3 and see what happens.
+  lsp.configure('solidity', {
+    settings = {
+      solidity = { includePath = '', remapping = { ["@OpenZeppelin/"] = 'dependencies/OpenZeppelin/openzeppelin-contracts@4.6.0/' } }
+    }
+  })
 
-lsp.setup_nvim_cmp({
-  sources = sources
-})
+  lsp.nvim_workspace()
 
-lsp.setup()
+  local sources = require('lsp-zero').defaults.cmp_sources()
 
+  table.insert(sources, { name = "calc" })
+
+  lsp.setup_nvim_cmp({
+    sources = sources,
+    mapping = cmp_mappings
+  })
+
+  lsp.setup()
