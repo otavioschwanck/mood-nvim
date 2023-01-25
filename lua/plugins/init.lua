8f157cb43c0c134a8af648936a73910bfa387696
Otávio Schwanck dos Santos <otavioschwanck@gmail.com>
2023-01-25 17:34:24 +0000

    improve context

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -78,7 +78,6 @@ local plugins = {
     require("indent_blankline").setup {
       space_char_blankline = " ",
       show_current_context = true,
-      show_current_context_start = true,
     }
   end },
   { 'danilamihailov/beacon.nvim' },
