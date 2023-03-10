b2be8f1ae8fcd81b790b2c69f81fcc6fb6e5d859
Otávio Schwanck dos Santos <otavioschwanck@gmail.com>
2023-03-10 12:48:40 +0000

    fix chatGPT

--- handbook.md
+++ handbook.md
@@ -352,6 +352,7 @@ Press `SPC i` to open ChatGPT
 |---------|---------------------------------------|
 | <C-c>   | to close chat window.                 |
 | <C-u>   | scroll up chat window.                |
+| <C-s>   | Submit message.                       |
 | <C-d>   | scroll down chat window.              |
 | <C-y>   | to copy/yank last answer.             |
 | <C-o>   | Toggle settings window.               |
--- lua/plugins/chatgpt.lua
+++ lua/plugins/chatgpt.lua
@@ -7,7 +7,20 @@ return {
     config = function()
       require("chatgpt").setup({
         welcome_message = "",
-
+        keymaps = {
+          close = { "<C-c>" },
+          submit = "<C-s>",
+          yank_last = "<C-y>",
+          scroll_up = "<C-u>",
+          scroll_down = "<C-d>",
+          toggle_settings = "<C-o>",
+          new_session = "<C-n>",
+          cycle_windows = "<Tab>",
+          -- in the Sessions pane
+          select_session = "<Space>",
+          rename_session = "r",
+          delete_session = "d",
+        },
       })
     end,
     requires = {
