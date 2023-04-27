f7f22d401603ecfa5d3321bdcfb2d1fd67d54223
Otávio Schwanck dos Santos <otavioschwanck@gmail.com>
2023-04-28 00:37:28 +0100

    disable codeium on telescope

--- lua/plugins/codeium.lua
+++ lua/plugins/codeium.lua
@@ -3,6 +3,7 @@ return {
     "Exafunction/codeium.vim",
     config = function()
       vim.g.codeium_disable_bindings = 1
+      vim.g.codeium_filetypes = { TelescopePrompt = false }
 
       vim.keymap.set('i', '<C-j>', function() return vim.fn['codeium#Accept']() end, { expr = true })
       vim.keymap.set('i', '<C-f>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true })
