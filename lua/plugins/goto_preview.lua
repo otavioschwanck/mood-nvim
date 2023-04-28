31dc6c434623d6e3d09314c115568a8df7eb95e4
Otávio Schwanck dos Santos <otavioschwanck@gmail.com>
2023-04-28 15:14:12 +0100

    improve preview shortcut

--- lua/plugins/goto_preview.lua
+++ lua/plugins/goto_preview.lua
@@ -2,11 +2,8 @@ return {
   {
     'rmagatti/goto-preview',
     keys = {
-      { 'gpd', '<cmd>lua require("goto-preview").goto_preview_definition()<CR>', desc = "Preview Definition", mode = 'n' },
-      { 'gpt', '<cmd>lua require("goto-preview").goto_preview_type_definition()<CR>', mode = 'n', desc = "Preview Type Definition" },
-      { 'gpi', '<cmd>lua require("goto-preview").goto_preview_implementation()<CR>', mode = 'n', desc = "Preview Implementation" },
+      { 'gp', '<cmd>lua require("goto-preview").goto_preview_definition()<CR>', desc = "Preview Definition", mode = 'n' },
       { 'gP', '<cmd>lua require("goto-preview").close_all_win()<CR>', mode = 'n', desc = "Close all preview windows" },
-      { 'gpr', '<cmd>lua require("goto-preview").goto_preview_references()<CR>', mode = 'n', desc = "Preview References" },
 
     },
     config = function()
