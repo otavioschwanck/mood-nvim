b0d838ea514a76c2e706269e2a0d115a46c43aad
Otávio Schwanck dos Santos <otavioschwanck@gmail.com>
2023-07-06 00:12:11 +0100

    remove highlight on yank

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -28,14 +28,6 @@ function M.setup()
     command = "checktime",
   })
 
-  -- Highlight on yank
-  vim.api.nvim_create_autocmd("TextYankPost", {
-    group = augroup("highlight_yank"),
-    callback = function()
-      vim.highlight.on_yank()
-    end,
-  })
-
   -- resize splits if window got resized
   vim.api.nvim_create_autocmd({ "VimResized" }, {
     group = augroup("resize_splits"),
