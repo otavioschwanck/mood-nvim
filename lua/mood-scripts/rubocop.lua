3e790ff93300c2a966f21165f9aea8ebbb1e99da
Otávio Schwanck dos Santos <otavioschwanck@gmail.com>
2023-07-18 09:39:17 +0100

    improve rubocop (SPC m d)

--- lua/mood-scripts/rubocop.lua
+++ lua/mood-scripts/rubocop.lua
@@ -1,23 +1,23 @@
 vim.notify = require("notify")
 
 local function filter_inplace(arr, func)
-    local new_index = 1
-    local size_orig = #arr
-    for old_index, v in ipairs(arr) do
-        if func(v, old_index) then
-            arr[new_index] = v
-            new_index = new_index + 1
-        end
+  local new_index = 1
+  local size_orig = #arr
+  for old_index, v in ipairs(arr) do
+    if func(v, old_index) then
+      arr[new_index] = v
+      new_index = new_index + 1
     end
-    for i = new_index, size_orig do arr[i] = nil end
+  end
+  for i = new_index, size_orig do arr[i] = nil end
 end
 
 function Split(s, delimiter)
-    local result = {};
-    for match in (s..delimiter):gmatch("(.-)"..delimiter) do
-        table.insert(result, match);
-    end
-    return result;
+  local result = {};
+  for match in (s .. delimiter):gmatch("(.-)" .. delimiter) do
+    table.insert(result, match);
+  end
+  return result;
 end
 
 local function comment_rubocop()
@@ -26,7 +26,7 @@ local function comment_rubocop()
   local bufnr = vim.fn.bufnr()
   local current_error
 
----@diagnostic disable-next-line: unused-local
+  ---@diagnostic disable-next-line: unused-local
   for __, v in ipairs(error) do
     if v.lnum + 1 == line and bufnr == v.bufnr then
       if not current_error then
@@ -38,9 +38,9 @@ local function comment_rubocop()
   if current_error then
     local message = current_error.message
     local current_line = vim.fn.getline(".")
-    local parsed_message = Split(message, " ")[1]
-    parsed_message = string.gsub(parsed_message, "%[", "")
-    parsed_message = string.gsub(parsed_message, "%]", "")
+    local parsed_message = Split(message, "%[")
+    parsed_message = parsed_message[#parsed_message]
+    parsed_message = string.sub(parsed_message, 1, -3)
 
     if string.match(current_line, "# rubocop:disable") then
       vim.cmd("normal! A, " .. parsed_message)
