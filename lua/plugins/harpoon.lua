dde8f90f53a786d6656d6bc54717a9a75165b088
Otávio Schwanck dos Santos <otavioschwanck@gmail.com>
2023-09-20 20:30:06 +0100

    global harpoon instead for branch

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,20 +1,18 @@
 return {
-  {
-    'otavioschwanck/harpoon',
-    config = function()
-      require('harpoon').setup(
-        {
-          global_settings = {
-            mark_branch = true,
-            tabline = true,
-            tabline_icons = true,
-            tabline_prefix = '   '
-          }
-        }
-      )
-    end,
-    keys = {
-      { '<C-s>', '<cmd>lua require("harpoon.mark").toggle_file()<cr>', desc = 'Pin on Harpoon' },
-    }
-  }
+	{
+		"otavioschwanck/harpoon",
+		config = function()
+			require("harpoon").setup({
+				global_settings = {
+					mark_branch = false,
+					tabline = true,
+					tabline_icons = true,
+					tabline_prefix = "   ",
+				},
+			})
+		end,
+		keys = {
+			{ "<C-s>", '<cmd>lua require("harpoon.mark").toggle_file()<cr>', desc = "Pin on Harpoon" },
+		},
+	},
 }
