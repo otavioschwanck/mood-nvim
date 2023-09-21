07dd62c57b8b11d18a3bc716c9d9cf07f8a3a1db
Otávio Schwanck dos Santos <otavioschwanck@gmail.com>
2023-09-21 18:19:52 +0100

    improvement

--- lua/mood-scripts/setup-telescope.lua
+++ lua/mood-scripts/setup-telescope.lua
@@ -69,6 +69,7 @@ function M.setup()
 			file_browser = {
 				hidden = true,
 				prompt_path = true,
+				hide_parent_dir = true,
 				mappings = {
 					["i"] = {
 						["<C-o>"] = fb_actions.remove,
