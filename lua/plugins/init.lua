ec6f8ca113cedb78a423fa29a85fa338bbe44c2f
Otávio Schwanck dos Santos <otavioschwanck@gmail.com>
2023-09-29 14:40:07 +0100

    improve delimiters

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -154,7 +154,6 @@ local plugins = {
 	"vim-ruby/vim-ruby",
 	"farmergreg/vim-lastplace",
 	"svermeulen/vim-yoink",
-	"p00f/nvim-ts-rainbow",
 	"tpope/vim-fugitive",
 	"AndrewRadev/undoquit.vim",
 	"michaeljsmith/vim-indent-object",
@@ -228,23 +227,41 @@ local plugins = {
 			})
 		end,
 	},
-	{
-		"lukas-reineke/indent-blankline.nvim",
-		config = function()
-			vim.g.indent_blankline_filetype_exclude = {
-				"help",
-				"dashboard",
-				"NvimTree",
-			}
-
-			require("indent_blankline").setup({
-				space_char_blankline = " ",
-				show_current_context = true,
-			})
-		end,
-	},
 	{ "moll/vim-bbye" },
 	{ "otavioschwanck/ruby-toolkit.nvim" },
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		main = "ibl",
+		config = function()
+			local highlight = {
+				"RainbowRed",
+				"RainbowYellow",
+				"RainbowBlue",
+				"RainbowOrange",
+				"RainbowGreen",
+				"RainbowViolet",
+				"RainbowCyan",
+			}
+			local hooks = require("ibl.hooks")
+			-- create the highlight groups in the highlight setup hook, so they are reset
+			-- every time the colorscheme changes
+			hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+				vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
+				vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
+				vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
+				vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
+				vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
+				vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+				vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+			end)
+
+			vim.g.rainbow_delimiters = { highlight = highlight }
+			require("ibl").setup({ scope = { highlight = highlight } })
+
+			hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
+		end,
+	},
+	"HiPhish/rainbow-delimiters.nvim",
 	{
 		"lewis6991/gitsigns.nvim",
 		config = function()
--- lua/plugins/tree-sitter.lua
+++ lua/plugins/tree-sitter.lua
@@ -1,111 +1,115 @@
 return {
-  'nvim-treesitter/nvim-treesitter-textobjects',
-  'RRethy/nvim-treesitter-endwise',
-  { 'nvim-treesitter/nvim-treesitter', build = ':TSUpdate', config = function()
-    require 'nvim-treesitter.configs'.setup {
-      ensure_installed = {
-        "ruby",
-        "html",
-        "css",
-        "scss",
-        "javascript",
-        "typescript",
-        "solidity",
-        "yaml",
-        "json",
-        "lua",
-        "vim",
-        "query",
-        "embedded_template",
-        "bash"
-      },
+	"nvim-treesitter/nvim-treesitter-textobjects",
+	"RRethy/nvim-treesitter-endwise",
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				ensure_installed = {
+					"ruby",
+					"html",
+					"css",
+					"scss",
+					"javascript",
+					"typescript",
+					"solidity",
+					"yaml",
+					"json",
+					"lua",
+					"vim",
+					"query",
+					"embedded_template",
+					"bash",
+				},
 
-      auto_install = true,
+				auto_install = true,
 
-      textobjects = {
-        move = {
-          enable = true,
-          set_jumps = true, -- whether to set jumps in the jumplist
-          goto_next_start = {
-            ["]m"] = "@function.outer",
-            ["]]"] = "@class.outer"
-          },
-          goto_next_end = {
-            ["]M"] = "@function.outer",
-            ["]["] = "@class.outer",
-          },
-          goto_previous_start = {
-            ["[m"] = "@function.outer",
-            ["[["] = "@class.outer",
-          },
-          goto_previous_end = {
-            ["[M"] = "@function.outer",
-            ["[]"] = "@class.outer",
-          },
-        },
-        rainbow = { enabled = true, extended_mode = true, max_file_lines = 1500 },
-        select = {
-          enable = true,
+				textobjects = {
+					move = {
+						enable = true,
+						set_jumps = true, -- whether to set jumps in the jumplist
+						goto_next_start = {
+							["]m"] = "@function.outer",
+							["]]"] = "@class.outer",
+						},
+						goto_next_end = {
+							["]M"] = "@function.outer",
+							["]["] = "@class.outer",
+						},
+						goto_previous_start = {
+							["[m"] = "@function.outer",
+							["[["] = "@class.outer",
+						},
+						goto_previous_end = {
+							["[M"] = "@function.outer",
+							["[]"] = "@class.outer",
+						},
+					},
+					select = {
+						enable = true,
 
-          keymaps = {
-            ["af"] = "@function.outer",
-            ["if"] = "@function.inner",
-            ["ac"] = "@class.outer",
-            ["ic"] = "@class.inner",
-          },
-        },
-      },
+						keymaps = {
+							["af"] = "@function.outer",
+							["if"] = "@function.inner",
+							["ac"] = "@class.outer",
+							["ic"] = "@class.inner",
+						},
+					},
+				},
 
-      endwise = {
-        enable = true,
-      },
+				endwise = {
+					enable = true,
+				},
 
+				-- List of parsers to ignore installing
+				ignore_install = { "phpdoc" },
 
-      -- List of parsers to ignore installing
-      ignore_install = { "phpdoc" },
+				autotag = {
+					enable = true,
+				},
+				-- Install languages synchronously (only applied to `ensure_installed`)
+				sync_install = false,
 
-      autotag = {
-        enable = true,
-      },
-      -- Install languages synchronously (only applied to `ensure_installed`)
-      sync_install = false,
+				indent = { enable = true, disable = { "ruby", "python" } },
 
-      indent = { enable = true, disable = { "ruby", "python" } },
+				highlight = {
+					enable = true,
+				},
+			})
+		end,
+	},
+	{
+		"nvim-treesitter/nvim-treesitter-context",
+		config = function()
+			require("treesitter-context").setup({
+				enable = true,
+				max_lines = 0,
+				trim_scope = "outer",
+				patterns = { -- Match patterns for TS nodes. These get wrapped to match at word boundaries.
+					-- For all filetypes
+					-- Note that setting an entry here replaces all other patterns for this entry.
+					-- By setting the 'default' entry below, you can control which nodes you want to
+					-- appear in the context window.
+					default = {
+						"class",
+						"function",
+						"method",
+						"for", -- These won't appear in the context
+						"while",
+						"if",
+						"switch",
+						"case",
+						"element",
+						"call",
+					},
+				},
+				exact_patterns = {},
 
-      highlight = {
-        enable = true,
-      },
-    }
-  end
-  },
-  { "nvim-treesitter/nvim-treesitter-context", config = function()
-    require 'treesitter-context'.setup {
-      enable = true,
-      max_lines = 0,
-      trim_scope = 'outer',
-      patterns = { -- Match patterns for TS nodes. These get wrapped to match at word boundaries.
-        -- For all filetypes
-        -- Note that setting an entry here replaces all other patterns for this entry.
-        -- By setting the 'default' entry below, you can control which nodes you want to
-        -- appear in the context window.
-        default = {
-          'class',
-          'function',
-          'method',
-          'for', -- These won't appear in the context
-          'while',
-          'if',
-          'switch',
-          'case',
-          'element',
-          'call'
-        },
-      },
-      exact_patterns = {},
-
-      zindex = 20, -- The Z-index of the context window
-      mode = 'cursor', -- Line used to calculate context. Choices: 'cursor', 'topline'
-      separator = nil, -- Separator between context and content. Should be a single character string, like '-'.
-    }
-  end }
+				zindex = 20, -- The Z-index of the context window
+				mode = "cursor", -- Line used to calculate context. Choices: 'cursor', 'topline'
+				separator = nil, -- Separator between context and content. Should be a single character string, like '-'.
+			})
+		end,
+	},
 }
