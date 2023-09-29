8cf963f46b794d8d8327b403f68e9531e3edcfe2
Otávio Schwanck dos Santos <otavioschwanck@gmail.com>
2023-09-29 17:56:50 +0100

    fix business spec

--- lua/utils/insert_rspec.lua
+++ lua/utils/insert_rspec.lua
@@ -3,30 +3,30 @@ local toCamel = require("utils.table_functions").camelCaseB
 local singularize = require("utils.table_functions").singularize
 
 local function insert_test_template()
-  local relative_path = vim.fn.fnamemodify(vim.fn.expand("%"), ":~:.")
-  local splitted_path = split(relative_path, "/")
-  local class_name = ""
+	local relative_path = vim.fn.fnamemodify(vim.fn.expand("%"), ":~:.")
+	local splitted_path = split(relative_path, "/")
+	local class_name = ""
 
-  for i = 3, #splitted_path do
-    class_name = class_name .. toCamel(split(splitted_path[i], "_spec.rb")[1])
+	for i = 3, #splitted_path do
+		class_name = class_name .. toCamel(split(splitted_path[i], "_spec.rb")[1])
 
-    if i < #splitted_path then
-      class_name = class_name .. "::"
-    end
-  end
+		if i < #splitted_path then
+			class_name = class_name .. "::"
+		end
+	end
 
-  local type = singularize(splitted_path[2])
+	local type = singularize(splitted_path[2])
 
-  local text_to_insert = {
-    'require "rails_helper"',
-    '',
-    'RSpec.describe ' .. class_name .. ', type: :' .. type .. ' do',
-    '',
-    'end'
-  }
+	local text_to_insert = {
+		'require "rails_helper"',
+		"",
+		"RSpec.describe " .. class_name .. ", type: :" .. type .. " do",
+		"",
+		"end",
+	}
 
-  vim.api.nvim_buf_set_lines(0, 0, 4, false, text_to_insert)
-  vim.fn.feedkeys("3jI  ")
+	vim.api.nvim_buf_set_lines(0, 0, 4, false, text_to_insert)
+	vim.fn.feedkeys("3jI  ")
 end
 
 return insert_test_template
--- lua/utils/table_functions.lua
+++ lua/utils/table_functions.lua
@@ -1,13 +1,13 @@
 local M = {}
 
 function M.Split(s, delimiter)
-  local result = {};
+	local result = {}
 
-  for match in (s..delimiter):gmatch("(.-)"..delimiter) do
-    table.insert(result, match);
-  end
+	for match in (s .. delimiter):gmatch("(.-)" .. delimiter) do
+		table.insert(result, match)
+	end
 
-  return result;
+	return result
 end
 
 function M.camelCaseB(s)
@@ -17,50 +17,57 @@ function M.camelCaseB(s)
 		return string.upper(first) .. rest
 	end)
 
-  return string.upper(string.sub(camel, 1, 1)) .. string.sub(camel, 2, #camel)
+	return string.upper(string.sub(camel, 1, 1)) .. string.sub(camel, 2, #camel)
 end
 
 M.pluralize = function(inputString)
-  local lastCharacter = inputString:sub(-1, -1)
+	local lastCharacter = inputString:sub(-1, -1)
 
-  if lastCharacter == "y" then
-    local stringWithoutY = inputString:sub(1, -2)
+	if lastCharacter == "y" then
+		local stringWithoutY = inputString:sub(1, -2)
 
-    return stringWithoutY .. "ies"
-  elseif lastCharacter == "s" then
-    return inputString
-  else
-    return inputString .. "s"
-  end
+		return stringWithoutY .. "ies"
+	elseif lastCharacter == "s" then
+		return inputString
+	else
+		return inputString .. "s"
+	end
 end
 
 M.singularize = function(inputString)
-  local lastCharacter = inputString:sub(-1, -1)
+	local lastCharacter = inputString:sub(-1, -1)
 
-  local lastThreeCharacters = ""
+	local lastThreeCharacters = ""
+	local lastTwoCharacters = ""
 
-  if #inputString >= 3 then
-    lastThreeCharacters = inputString:sub(-3, -1)
-  end
+	if #inputString >= 3 then
+		lastThreeCharacters = inputString:sub(-3, -1)
+	end
 
-  if lastThreeCharacters == "ies" then
-    local stringWithoutIes = inputString:sub(1, -4)
+	if #inputString >= 2 then
+		lastTwoCharacters = inputString:sub(-2, -1)
+	end
 
-    return stringWithoutIes .. "y"
-  elseif lastCharacter == "s" then
-    return inputString:sub(1, -2)
-  else
-    return inputString
-  end
+	if lastThreeCharacters == "ies" then
+		local stringWithoutIes = inputString:sub(1, -4)
+
+		return stringWithoutIes .. "y"
+	elseif lastTwoCharacters == "ss" then
+		return inputString
+	elseif lastCharacter == "s" then
+		return inputString:sub(1, -2)
+	else
+		return inputString
+	end
 end
 
 function M.SplitWithDot(s)
-  local lines = {}
-  for line in s:gsub("%f[.]%.%f[^.]", "\0"):gmatch"%Z+" do
-    table.insert(lines, line)
-  end
+	local lines = {}
+	for line in s:gsub("%f[.]%.%f[^.]", "\0"):gmatch("%Z+") do
+		table.insert(lines, line)
+	end
 
-  return lines
+	return lines
 end
 
 return M
