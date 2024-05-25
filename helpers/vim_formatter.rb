e61aa96f488da94396fa4a578bbe00e2ebcedb33
Otávio Schwanck dos Santos <otavioschwanck@gmail.com>
2024-05-25 12:53:44 +0100

    improve spec

--- helpers/vim_formatter.rb
+++ helpers/vim_formatter.rb
@@ -18,7 +18,30 @@ class VimFormatter
   private
 
   def format(notification)
-    rtn = "%s: %s" % [notification.example.location, notification.exception.message.gsub("\n", "\\n")]
+    example_location = notification.example.location
+    expect_location = extract_expect_location(notification.exception.backtrace, example_location)
+    message = notification.exception.message.gsub("\n", "\\n")
+    rtn = "%s: %s" % [expect_location || example_location, message]
     rtn.gsub("\n", ' ')
+  end
+
+  def extract_expect_location(backtrace, example_location)
+    backtrace_line = backtrace.find { |line| line.include?('_spec.rb') }
+    return nil unless backtrace_line
+
+    example_file = example_location.split(':').first
+
+    match = backtrace_line.match(/(.+_spec\.rb):(\d+)/)
+    return nil unless match
+
+    backtrace_file = match[1]
+    backtrace_line_number = match[2]
+
+    if File.basename(example_file) == File.basename(backtrace_file)
+      relative_path = Pathname.new(backtrace_file).relative_path_from(Pathname.new(Dir.pwd)).to_s
+      "#{relative_path}:#{backtrace_line_number}"
+    end
+  rescue StandardError
+    # do nothing
   end
 end
