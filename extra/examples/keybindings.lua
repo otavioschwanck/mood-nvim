b514ad8a37add6780a071d7d4f295f0542620fd5
Otávio Schwanck dos Santos <otavioschwanck@gmail.com>
2024-09-21 12:14:03 +0100

    fix keybindings

--- extra/examples/keybindings.lua
+++ extra/examples/keybindings.lua
@@ -7,9 +7,6 @@ local find_in_folder = require('helpers.user-functions').find_on_folder
 local wk = require("which-key")
 local tmux = require('tmux-awesome-manager.src.term')
 
--- Format on save
-vim.g.format_on_save = true -- Configure formatter on SPC f p -> LSP
-
 -- Arguments for tmux.run_wk:
 --  opts.focus_when_call -- Focus terminal instead opening a enw one - default = true
 --  opts.visit_first_call -- Focus the new opened window / pane. default = true
@@ -21,35 +18,35 @@ vim.g.format_on_save = true -- Configure formatter on SPC f p -> LSP
 
 -- Registering your keybindings for SPC key.
 wk.add({
-  { "<leader>=",   ":w | :silent !bundle exec rubocop -A %<CR>:e %<CR>",                                                                                                       desc = "Rubocop on current file", silent = false },
-  { "<leader>+",   tmux.run_wk({ cmd = 'bundle exec rubocop -A', name = 'rubocop', open_as = 'pane', close_on_timer = 2, visit_first_call = false, focus_when_call = false }), desc = "rubocop",                 silent = false },
+  { "<leader>=",   ":w | :silent !bundle exec rubocop -A %<CR>:e %<CR>",                                                                                                    desc = "Rubocop on current file" },
+  { "<leader>+",   tmux.run({ cmd = 'bundle exec rubocop -A', name = 'rubocop', open_as = 'pane', close_on_timer = 2, visit_first_call = false, focus_when_call = false }), desc = "rubocop" },
 
-  { "<leader>o",   group = "Term Commands",                                                                                                                                    silent = false },
-  { "<leader>o1",  tmux.run_wk({ cmd = 'docker-compose up -d', name = 'Docker Compose up' }),                                                                                  desc = "Docker Compose up",       silent = false },
+  { "<leader>o",   group = "Term Commands" },
+  { "<leader>o1",  tmux.run({ cmd = 'docker-compose up -d', name = 'Docker Compose up' }),                                                                                  desc = "Docker Compose up" },
   -- Yarn commands
-  { "<leader>oy",  group = "yarn",                                                                                                                                             silent = false },
-  { "<leader>oyi", tmux.run_wk({ cmd = 'yarn install', name = 'Yarn Install' }),                                                                                               desc = "Yarn Install",            silent = false },
-  { "<leader>oya", tmux.run_wk({ cmd = 'yarn add %1', name = 'Yarn Add', questions = { { question = 'package name: ', required = true } } }),                                  desc = "Yarn Add",                silent = false },
-  { "<leader>oyd", tmux.run_wk({ cmd = 'yarn dev', name = 'Yarn Dev' }),                                                                                                       desc = "Yarn Dev",                silent = false },
+  { "<leader>oy",  group = "yarn" },
+  { "<leader>oyi", tmux.run({ cmd = 'yarn install', name = 'Yarn Install' }),                                                                                               desc = "Yarn Install" },
+  { "<leader>oya", tmux.run({ cmd = 'yarn add %1', name = 'Yarn Add', questions = { { question = 'package name: ', required = true } } }),                                  desc = "Yarn Add" },
+  { "<leader>oyd", tmux.run({ cmd = 'yarn dev', name = 'Yarn Dev' }),                                                                                                       desc = "Yarn Dev" },
 
   -- Rubocop on current file
-  { "<leader>or",  ":silent !bundle exec rubocop -a %<CR>",                                                                                                                    desc = "Rubocop on current file", silent = false },
+  { "<leader>or",  ":silent !bundle exec rubocop -a %<CR>",                                                                                                                 desc = "Rubocop on current file" },
 
   -- Brownie commands
-  { "<leader>ob",  group = "Brownie",                                                                                                                                          silent = false },
-  { "<leader>obt", tmux.run_wk({ cmd = 'brownie test', name = 'Brownie Test' }),                                                                                               desc = "Brownie Test",            silent = false },
-  { "<leader>obC", tmux.run_wk({ cmd = 'brownie compile', name = 'Brownie Compile' }),                                                                                         desc = "Brownie Compile",         silent = false },
-  { "<leader>obc", tmux.run_wk({ cmd = 'brownie console', name = 'brownie console' }),                                                                                         desc = "brownie console",         silent = false },
+  { "<leader>ob",  group = "Brownie" },
+  { "<leader>obt", tmux.run({ cmd = 'brownie test', name = 'Brownie Test' }),                                                                                               desc = "Brownie Test" },
+  { "<leader>obC", tmux.run({ cmd = 'brownie compile', name = 'Brownie Compile' }),                                                                                         desc = "Brownie Compile" },
+  { "<leader>obc", tmux.run({ cmd = 'brownie console', name = 'brownie console' }),                                                                                         desc = "brownie console" },
 
   -- Rails commands
-  { "<leader>r",   group = "Rails",                                                                                                                                            silent = false },
-  { "<leader>rr",  tmux.run_wk({ cmd = 'rails c', name = 'Rails Console', close_on_timer = 3 }),                                                                               desc = "Rails Console",           silent = false },
-  { "<leader>rR",  tmux.run_wk({ cmd = 'rails s', name = 'Rails Server', visit_first_call = false, open_as = 'window' }),                                                      desc = "Rails Server",            silent = false },
-  { "<leader>rb",  tmux.run_wk({ cmd = 'bundle install', name = 'Bundle Install', open_as = 'pane', close_on_timer = 2, visit_first_call = false, focus_when_call = false }),  desc = "Bundle Install",          silent = false },
-  { "<leader>rg",  tmux.run_wk({ cmd = 'rails generate %1', name = 'Rails Generate', questions = { { question = "Rails generate: ", required = true } } }),                    desc = "Rails Generate",          silent = false },
-  { "<leader>rd",  tmux.run_wk({ cmd = 'rails destroy %1', name = 'Rails Destroy', questions = { { question = "Rails destroy: ", required = true } } }),                       desc = "Rails Destroy",           silent = false },
-  { "<leader>ri",  tmux.run_wk({ cmd = 'rails db:migrate', name = 'Rails db:migrate' }),                                                                                       desc = "Rails db:migrate",        silent = false },
-  { "<leader>rI",  ":call ResetRailsDb('bin/rails db:environment:set RAILS_ENV=development; rails db:drop db:create db:migrate;rails db:seed')<CR>",                           desc = "Rails Reset DB",          silent = false },
+  { "<leader>r",   group = "Rails" },
+  { "<leader>rr",  tmux.run({ cmd = 'rails c', name = 'Rails Console', close_on_timer = 3 }),                                                                               desc = "Rails Console" },
+  { "<leader>rR",  tmux.run({ cmd = 'rails s', name = 'Rails Server', visit_first_call = false, open_as = 'window' }),                                                      desc = "Rails Server" },
+  { "<leader>rb",  tmux.run({ cmd = 'bundle install', name = 'Bundle Install', open_as = 'pane', close_on_timer = 2, visit_first_call = false, focus_when_call = false }),  desc = "Bundle Install" },
+  { "<leader>rg",  tmux.run({ cmd = 'rails generate %1', name = 'Rails Generate', questions = { { question = "Rails generate: ", required = true } } }),                    desc = "Rails Generate" },
+  { "<leader>rd",  tmux.run({ cmd = 'rails destroy %1', name = 'Rails Destroy', questions = { { question = "Rails destroy: ", required = true } } }),                       desc = "Rails Destroy" },
+  { "<leader>ri",  tmux.run({ cmd = 'rails db:migrate', name = 'Rails db:migrate' }),                                                                                       desc = "Rails db:migrate" },
+  { "<leader>rI",  ":call ResetRailsDb('bin/rails db:environment:set RAILS_ENV=development; rails db:drop db:create db:migrate;rails db:seed')<CR>",                        desc = "Rails Reset DB" },
 
   -- Rails file finders
   find_in_folder("<leader>rm", 'app/models', 'Find Model'),
@@ -65,9 +62,9 @@ wk.add({
   find_in_folder("<leader>rM", 'db/migrate', 'Find Migration'),
 
   -- Miscellaneous Rails commands
-  { "<leader>rM", ":Emodel<CR>",                   desc = "Find Model",             silent = false },
-  { "<leader>rC", ":Econtroller<CR>",              desc = "Find Controller",        silent = false },
-  { "<leader>rK", ":call KillRubyInstances()<CR>", desc = "Kill Ruby Instances",    silent = false },
-  { "<leader>rU", ":Efixtures<CR>",                desc = "Find Current Fixture",   silent = false },
-  { "<leader>rN", ":Emigration<CR>",               desc = "Find Current Migration", silent = false }
+  { "<leader>rM", ":Emodel<CR>",                   desc = "Find Model" },
+  { "<leader>rC", ":Econtroller<CR>",              desc = "Find Controller" },
+  { "<leader>rK", ":call KillRubyInstances()<CR>", desc = "Kill Ruby Instances" },
+  { "<leader>rU", ":Efixtures<CR>",                desc = "Find Current Fixture" },
+  { "<leader>rN", ":Emigration<CR>",               desc = "Find Current Migration" }
 })
