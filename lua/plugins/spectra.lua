2c1b90f5c54b4cc3b8fcb8ff5c4df1ef3ed0822a
Otávio Schwanck dos Santos <otavioschwanck@gmail.com>
2025-02-12 12:29:31 +0000

    fix spectre

--- lua/plugins/spectra.lua
+++ lua/plugins/spectra.lua
@@ -1,10 +1,30 @@
 return {
-	{
-		"windwp/nvim-spectre",
-		event = "VeryLazy",
-    -- stylua: ignore
+  {
+    "windwp/nvim-spectre",
+    event = "VeryLazy",
+    config = function()
+      local args = {}
+      local is_mac = vim.loop.os_uname().sysname == "Darwin"
+
+      if is_mac then
+        args = {
+          replace_engine = {
+            ["sed"] = {
+              cmd = "sed",
+              args = {
+                "-i",
+                "",
+                "-E",
+              }
+            },
+          }
+        }
+      end
+
+      require("spectre").setup({})
+    end,
     keys = {
       { "<leader>sr", function() require("spectre").open() end, desc = "Replace in files (Spectre)" },
     },
-	},
+  },
 }
